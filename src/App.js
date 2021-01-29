de030ccc434c7374aa6d3791785d87bbc9d85786
soon <qkrtnsgud0229@gmail.com>
2021-01-29 09:50:57 +0900

    04-4 음식 앱에 prop-types 도입하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import React from "react";
+
+function Food({ name, image }) {
+    console.log(image);
+    return (
+        <div>
+            <h2>I like {name}</h2>
+
+            <img src={image} alt={name} />
+        </div>
+    );
+}
+
+const foodILike = [
+    {
+        id: 1,
+        name: "raman",
+        image: "https://cdn.pixabay.com/photo/2019/11/23/15/26/ramen-4647408_960_720.jpg",
+        rating: 5,
+    },
+    {
+        id: 2,
+        name: "kimchi",
+        image: "https://cdn.pixabay.com/photo/2019/07/25/01/35/kimchi-4361465_960_720.jpg",
+        rating: 5,
+    },
+    {
+        id: 3,
+        name: "banana",
+        image: "https://cdn.pixabay.com/photo/2017/06/27/22/21/banana-2449019_960_720.jpg",
+        rating: 4,
+    },
+    {
+        id: 4,
+        name: "apple",
+        image: "https://cdn.pixabay.com/photo/2016/08/12/22/38/apple-1589874_960_720.jpg",
+        rating: 3,
+    },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {foodILike.map(dish => (
+                <Food key={dish.id} name={dish.name} image={dish.image} />
+            ))}
+        </div>
+    );
 }
 
 export default App;
